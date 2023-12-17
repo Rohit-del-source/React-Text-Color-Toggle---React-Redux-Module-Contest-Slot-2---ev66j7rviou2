b7844787104e72a0ac35c1b2c8f52c6f72fad061
Rohit-del-source <rohitverma18158920@gmail.com>
2023-12-17 18:20:41 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+  const [isBlue , setIsBlue] = useState(false);
+
+  const toogleColor = () => {
+    setIsBlue(!isBlue);
+  }
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={color} >Newton School</p>
+      <button id='button' onClick={toogleColor}>Change Style</button>
     </div>
   )
 }
